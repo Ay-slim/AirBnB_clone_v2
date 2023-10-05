55c0e3bf8fa1ecb0686ee9774b98770ef859c7d0
Ay-slim <ayooluwaadedipe@gmail.com>
2023-10-05 02:17:11 -0700

    Compress to tgz

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# Fabfile to compress web_static content into a .tgz archive
+
+from fabric.api import local
+from datetime import datetime
+import os.path
+
+
+def do_pack():
+    """
+    A fuction to compress web_static content into a .tgz archive
+    """
+    now = datetime.utcnow()
+    filename = "versions/web_static_{}.tgz".format(
+            now.strftime('%Y%m%d%H%M%S'))
+    if not os.path.isdir("versions"):
+        if local("mkdir -p versions").failed:
+            return None
+    print('Getting here')
+    print(filename, type(filename))
+    if local("tar -cvzf {} web_static".format(filename)).failed:
+        return None
+    return filename

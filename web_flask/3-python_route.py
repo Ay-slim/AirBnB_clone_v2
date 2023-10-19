50ad30c5d88819bf604cfb4bd4b5616643f29315
Ay-slim <ayooluwaadedipe@gmail.com>
2023-10-19 03:45:00 -0700

    Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Hello Flask route declaration module second endpoint"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Root route for Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def only_hbnb():
+    """/hbnb route definition"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_arg_hbnb(text):
+    """/c/<text> route definition"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_arg_hbnb(text="is cool"):
+    """/python/<text> route definition"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run()
